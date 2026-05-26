851feab14f5215f67da7232a8ed14f71b13cde7e
Julius Vendorf <juliusv135@gmail.com>
2026-05-26 15:10:19 +0200

    I'm good

--- env_debug.py
+++ env_debug.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Go up one dir and load .env file
-load_dotenv('auto-replies-export.env')
+load_dotenv('name_of_file.env')
 
 APP_ID = os.getenv('HELP_SCOUT_APP_ID')
 APP_SECRET = os.getenv('HELP_SCOUT_APP_SECRET')
